7656ecba237191e6663c5b9da57b91ae6f5bdef3
arhibale <arhibale@yandex.ru>
2021-08-05 22:39:59 +0400

    Initial commit

---
+++ android/src/com/arhibale/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.arhibale;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.arhibale.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
