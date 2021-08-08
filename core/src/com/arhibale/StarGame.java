032522e0f25b534ca50349c0f185540d7c26247f
arhibale <arhibale@yandex.ru>
2021-08-08 18:06:25 +0400

    complected hm2

--- core/src/com/arhibale/StarGame.java
+++ core/src/com/arhibale/StarGame.java
@@ -1,52 +1,14 @@
 package com.arhibale;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private Texture back;
+import com.arhibale.screen.MenuScreen;
 
-	private int x = 0;
-	private int y = 0;
+public class StarGame extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("anime.png");
-		back = new Texture(Gdx.files.internal("background.jpg"));
+		setScreen(new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		temporaryAnimation();
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(back, 0, 0);
-		batch.draw(img, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-		back.dispose();
-	}
-
-	private void temporaryAnimation() {
-		if (y == 0 && (x + 281) != Gdx.graphics.getWidth()) {
-			x++;
-		} else if ((x + 281) == Gdx.graphics.getWidth() && (y + 281) != Gdx.graphics.getHeight()) {
-			y++;
-		} else if ((y + 281) == Gdx.graphics.getHeight() && x != 0) {
-			x--;
-		} else {
-			y--;
-		}
-	}
 }
