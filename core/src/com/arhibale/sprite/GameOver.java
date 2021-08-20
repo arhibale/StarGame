c089b4129ec7c4d5ed113d63a7dfc8dbf719748b
arhibale <arhibale@yandex.ru>
2021-08-20 11:07:57 +0400

    complected hm 7

---
+++ core/src/com/arhibale/sprite/GameOver.java
@@ -0,0 +1,19 @@
+package com.arhibale.sprite;
+
+import com.arhibale.base.Sprite;
+import com.arhibale.math.Rect;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class GameOver extends Sprite {
+
+    public GameOver(TextureAtlas atlas) {
+        super(atlas.findRegion("message_game_over"));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(0.08f);
+        setBottom(worldBounds.getBottom() + 0.65f);
+    }
+}
